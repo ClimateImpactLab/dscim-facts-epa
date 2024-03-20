fb84d8058ea651b3ba34a42535cf8b64e328f030
JMGilbert <jonahmgilbert1@gmail.com>
2024-03-20 12:39:19 -0500

    Modularize inputs/outputs to allow for containerization

--- scripts/facts.runs/facts_runs.sh
+++ scripts/facts.runs/facts_runs.sh
@@ -5,10 +5,24 @@
 # example: gases=("CO2_Fossil" "CH4" "N2O")
 pulse_years=(2020)
 gases=("CO2_Fossil")
+# Desired GMSL pulse file path
+gmsl_file="/opt/inputs/climate/facts_gmsl_pulse.nc4"
+# Paths to directories
 facts_dir="/opt/facts"
 dscim_facts_epa_dir="/opt/dscim-facts-epa"
+inputs_dir="/opt/inputs"
+outputs_dir="/opt/outputs"
+configs_dir="/opt/configs"
+modules_dir="/opt/modules-data"
+# Creates symlink to all modules data
+ln -s $modules_dir/*.tar.gz $facts_dir/modules-data
 # Create FACTS experiments
-python3 prepare_facts.py --dscim_repo "${dscim_facts_epa_dir}" --facts_repo "${facts_dir}" --pulse_years "${pulse_years[@]}" --gases "${gases[@]}"
+python3 prepare_facts.py --dscim_repo "${dscim_facts_epa_dir}" \
+ --facts_repo "${facts_dir}" \
+ --pulse_years "${pulse_years[@]}" \
+ --gases "${gases[@]}" \
+ --gmst_file $inputs_dir/climate/gmst_pulse.nc4 \
+ --ohc_file $inputs_dir/climate/ohc_pulse.nc4 
 # Loop through the pulse years
 for gas in "${gases[@]}"; do
     # Loop through the gases
@@ -23,7 +37,7 @@ done
 python3 runFACTS.py rff.control.control
 cd $dscim_facts_epa_dir/scripts/facts.runs
 # Take the outputs of the FACTS experiment and save in the proper format
-python3 format_facts.py --facts_repo "${facts_dir}" --pulse_years "${pulse_years[@]}" --gases "${gases[@]}" --gmsl_pulse facts_gmsl_pulse.nc4
+python3 format_facts.py --facts_repo "${facts_dir}" --pulse_years "${pulse_years[@]}" --gases "${gases[@]}" --gmsl_file $gmsl_file
 cd $dscim_facts_epa_dir/scripts
 # Create config for dscim run
-python3 create_config.py --gmsl_file facts_gmsl_pulse.nc4 --gmst_file gmst_pulse.nc4 --pulse_years "${pulse_years[@]}" --gases "${gases[@]}"
+python3 create_config.py --gmsl_file $gmsl_file --gmst_file gmst_pulse.nc4 --pulse_years "${pulse_years[@]}" --gases "${gases[@]}"
